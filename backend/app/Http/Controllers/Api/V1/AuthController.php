254a7e6806061cc9a88eb188e5b40a2d895063d1
Loriane-2009 <loriane.magnenat@gmail.com>
2025-05-12 13:45:26 +0200

    feat: add auth controller

---
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users,email',
+            'password'=>'required|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name'=>$data['name'],
+            'email'=>$data['email'],
+            'password'=>Hash::make($data['password'])
+        ]);
+        $token = $user->createToken('my-app-token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response,201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => ['These credentials do not match our records.']
+            ], 404);
+        }
+
+        $token = $user->createToken('auth')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json($response,201);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        $response = [
+            'message'=>'logged out',
+        ];
+        return response($response);
+    }
+
+    public function me()
+    {
+        return new UserResource(auth()->user());
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ChapterController;
 use App\Http\Controllers\Api\V1\StoryController;
 use Illuminate\Support\Facades\Route;
@@ -7,4 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::apiResource('stories', StoryController::class);
     Route::apiResource('stories.chapters', ChapterController::class);
+    Route::post('/register',[AuthController::class,'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::delete('logout',[AuthController::class,'logout'])->name('logout');
+        Route::get('me',[AuthController::class,'me'])->name('me');
+    });
 });
