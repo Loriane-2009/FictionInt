17a291f1ffaafa78615311e2e847b6fcd2c9ccd4
Loriane-2009 <loriane.magnenat@gmail.com>
2025-05-18 13:36:30 +0200

    style: change background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="h-screen w-full bg-gray-100 grid grid-rows-[auto_1fr]">
-    <AppHeader/>
-    <div class="overflow-y-scroll">
-      <RouterView/>
+  <div class="h-screen w-full bg-fixed bg-cover bg-center app-background">
+    <div class="h-screen w-full grid grid-rows-[auto_1fr]">
+      <AppHeader/>
+      <div class="overflow-y-scroll">
+        <RouterView/>
+      </div>
     </div>
   </div>
 </template>
+<style>
+.app-background {
+  background-image: url('@/assets/background.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+</style>
 <script setup>
 import AppHeader from "@/components/AppHeader.vue";
 </script>
